a42ee79989d83a80ee60c3b8d61a0a19287d9817
Matt Jarvis <s3050285@student.rmit.edu.au>
2012-04-09 08:57:35 +1000

    small change, testing commit - matt

--- test.c
+++ test.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-int main(void){
+int main(void)
+{
+	int i;
 	printf("Token hello world first program\n");
+
+	/* matt's edit */
+	for(i = 0; i < 10; i++)
+	{
+		printf("%2d", i);
+	}
+	printf("\n");
+	/*end matt */
 	
 	return 1;
 }
